dad0bf4fb663ac824a2817a7e92ee02681bb129f
Anastasia <nastya-kotikm@yandex.ru>
2022-06-15 21:36:30 +0300

    Внесены изменения в файл Layout, подключены стили и скрипты Bootstrap, а также изменен заголовок title

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -3,22 +3,20 @@ const React = require('react');
 function Layout({ children }) {
   return (
     <html lang="ru">
-     <head>
-     <meta charSet="utf-8" />
-        {/* <link rel="stylesheet" href="/stylesheets/normalize.css" />
-        <link rel="stylesheet" href="/stylesheets/application.css" /> */}
+      <head>
+        <meta charSet="utf-8" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
 
+        {/* <link rel="stylesheet" type="text/css" href="/style.css" />  */}
+        {/* Не разобралась как правильно свои стили подключить :( */}
+
+        <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous" />
         <script defer src="/public/application" />
-
-        <title>Broccoli Blog</title>
+        <title>MAGIC: THE GATHERING</title>
         <meta charSet="utf-8" />
       </head>
       <body id="page-top">
-        <header>
-          {/* <div className="container px-4 px-lg-5 text-center"> */}
-            {children}
-         {/* </header> </div> */}
-        </header>
+        {children}
       </body>
     </html>
   );
