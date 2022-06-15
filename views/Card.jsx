60f0721b0c65b45f8fdacd758d36e39834b0be86
Anastasia <nastya-kotikm@yandex.ru>
2022-06-15 21:34:24 +0300

    Добавлен файл Card.js в папку Views, необходимый для отрисовки карточки

---
+++ views/Card.jsx
@@ -0,0 +1,22 @@
+const React = require('react');
+
+function Card({ card }) {
+  return (
+    <div className="col">
+      <div className="card" style={{ width: '18rem' }}>
+        <img src={card.url} className="card-img-top" alt="Cards" />
+        <div className="card-body">
+          <h5 className="card-title">{card.title}</h5>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">{card.condition}</li>
+          <li className="list-group-item">{card.price}</li>
+          <li className="list-group-item">{card.user_id}</li>
+        </ul>
+      </div>
+    </div>
+
+  );
+}
+
+module.exports = Card;
