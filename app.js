a0f0e26859e491ac73b0421be741deb82eec074b
Anastasia <nastya-kotikm@yandex.ru>
2022-06-15 22:20:00 +0300

    Внесены изменения в файл app.js для тестового запуска сервера

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const ReactDOMServer = require('react-dom/server');
 const React = require('react');
 
 const express = require('express');
-//const expressConfig = require('./config/express');
+// const expressConfig = require('./config/express');
 const logger = require('morgan');
 const path = require('path');
 
-//const session = require('express-session');
-//const FileStore = require('session-file-store')(session);
-//const cookieParser = require('cookie-parser');
+// const session = require('express-session');
+// const FileStore = require('session-file-store')(session);
+// const cookieParser = require('cookie-parser');
 
 // const sessionConfig = {
 //   store: new FileStore(),
 //   name: 'user_sid', // Имя куки для хранения id сессии. По умолчанию - connect.sid
-//   secret: process.env.SESSION_SECRET ?? 'test', // Секретное слово для шифрования, может быть любым
+//   secret: process.env.SESSION_SECRET ?? 'test', 
+// Секретное слово для шифрования, может быть любым
 //   resave: false, // Пересохранять ли куку при каждом запросе
 //   saveUninitialized: false, // Создавать ли сессию без инициализации ключей в req.session
 //   cookie: {
@@ -30,9 +31,8 @@ const homeRouter = require('./routes/views/home.routes');
 const app = express();
 const PORT = 3000;
 
-
-//app.use(cookieParser());
-//app.use(session(sessionConfig));
+// app.use(cookieParser());
+// app.use(session(sessionConfig));
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
@@ -45,12 +45,12 @@ app.listen(PORT, async () => {
   /* eslint-disable no-console */
   console.log('Веб-сервер слушает порт', PORT);
 
-  try {
-    await sequelize.authenticate();
-    console.log('БД-сервер подключен успешно');
-  } catch (error) {
-    console.log('БД-сервер не подключен');
-    console.log(error.message);
-  }
+  // try {
+  //   await sequelize.authenticate();
+  //   console.log('БД-сервер подключен успешно');
+  // } catch (error) {
+  //   console.log('БД-сервер не подключен');
+  //   console.log(error.message);
+  // }
   /* eslint-enable */
 });
