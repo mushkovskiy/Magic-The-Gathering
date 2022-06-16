b75129665439a6bb5e4ca730fc3c5ff45046d406
Anastasia <nastya-kotikm@yandex.ru>
2022-06-16 16:46:12 +0300

    Добавлен маршрут для записи карточки в базу данных

---
+++ routes/api/card.router.js
@@ -0,0 +1,16 @@
+const ReactDOMServer = require('react-dom/server');
+const React = require('react');
+const cardRouter = require('express').Router();
+
+const { Card } = require('../../db/models');
+
+cardRouter.post('/newCard', async (req, res) => {
+  await Card.create({
+    title: req.body.title,
+    url: req.body.url,
+    price: req.body.price,
+    condition: req.body.condition,
+  });
+});
+
+module.exports = cardRouter;
