175a7605d898f0df3090275ebf19eb1b105932f5
Anastasia <nastya-kotikm@yandex.ru>
2022-06-16 14:38:05 +0300

    Удалены лишние пустые строки и пробелы, а также изменены стили для тега body

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -9,20 +9,17 @@ function Layout({ children }) {
         {/* <link rel="stylesheet" href="/stylesheets/normalize.css" />
         <link rel="stylesheet" href="/stylesheets/application.css" /> */}
 
-
         {/* <link rel="stylesheet" type="text/css" href="/style.css" />  */}
         {/* Не разобралась как правильно свои стили подключить :( */}
 
         <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous" />
         <script defer src="/public/application" />
 
-
         <title>The Gathering</title>
         <meta charSet="utf-8" />
       </head>
       <body style={{
         display: 'flex',
-        justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
         width: '100%',
@@ -30,9 +27,7 @@ function Layout({ children }) {
       }}
       >
         <header>
-          {/* <div className="container px-4 px-lg-5 text-center"> */}
           {children}
-          {/* </header> </div> */}
         </header>
       </body>
     </html>
