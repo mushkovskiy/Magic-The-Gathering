fbab45ccb70e1b6964b06a462c6ff10a305174f2
Irina Tikhonova <102436907+Irenella7973@users.noreply.github.com>
2022-06-16 19:14:10 +0300

    Подключила logoutRouter и orderRouter

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ const { sequelize } = require('./db/models');
 const personRouter = require('./routes/views/person.routes');
 const authRouter = require('./routes/views/auth.routes');
 const homeRouter = require('./routes/views/home.routes');
+const logoutRouter = require('./routes/views/logout.routes');
+const orderRouter = require('./routes/views/order.routes');
 
 const app = express();
 
@@ -44,6 +46,8 @@ app.use(express.json());
 app.use(authRouter);
 app.use(homeRouter);
 app.use(personRouter);
+app.use(logoutRouter);
+app.use(orderRouter);
 
 app.listen(PORT, async () => {
   /* eslint-disable no-console */
