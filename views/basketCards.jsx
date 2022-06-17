0b353e66bb11b16febbf48aec55bd0afe9a40dd3
Eugene <eumu2022@gmail.com>
2022-06-17 10:58:36 +0300

    Добавлен файл basketCards

---
+++ views/basketCards.jsx
@@ -0,0 +1,29 @@
+const React = require('react');
+
+function Card({ card }) {
+  return (
+
+    <div className="col">
+      <div className="card" style={{ width: '18rem' }}>
+        <img src={card.url} className="card-img-top" alt="Cards" />
+        <div className="card-body">
+          <h5 className="card-title">{card.title}</h5>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">{card.condition}</li>
+          <li className="list-group-item">{card.price}</li>
+          <li className="list-group-item">{card.city}</li>
+          <li>
+            {' '}
+            <a href={`/add/${card.id}`}>
+              {' '}
+              <button type="submit" className="btn btn-primary">Войти</button>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Card;
