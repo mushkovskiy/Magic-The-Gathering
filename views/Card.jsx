ec635941d000f40c76d18174dd327a109a76fabe
Anastasia <nastya-kotikm@yandex.ru>
2022-06-17 15:09:49 +0300

    Добавлены заголовки полей в Home

--- views/Card.jsx
+++ views/Card.jsx
@@ -9,9 +9,9 @@ function Card({ card }) {
           <h5 className="card-title">{card.title}</h5>
         </div>
         <ul className="list-group list-group-flush">
-          <li className="list-group-item">{card.condition}</li>
-          <li className="list-group-item">{card.price}</li>
-          <li className="list-group-item">{card.user_id}</li>
+          <li className="list-group-item">{`Цена: ${card.price}`}</li>
+          <li className="list-group-item">{`Состояние: ${card.condition}`}</li>
+          {/* <li className="list-group-item">{card.user_id}</li> */}
         </ul>
       </div>
           <a href={`/add/${card.id}`}><button type="submit" className="btn btn-primary btn-add">Добавить в корзину</button></a>
